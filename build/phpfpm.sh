35e874f6734d2409af2504a574fb29bb822266d7
Roberto Silvino <robertosilvino@gmail.com>
2020-06-13 19:47:48 -0300

    atualização de diretórios para php 7.2 do alpine

--- build/phpfpm.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-# php7.0-fpm -c /etc/php/7.0/fpm/
-service php7.0-fpm start
--- install/run.image.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-sudo docker stop nginx-php70; sudo docker rm nginx-php70
-
-sudo docker run --name nginx-php70 \
-  -v "$PWD"/../conf:/etc/nginx/sites-available \
-  -v "$PWD"/../moodledata:/moodledata:rw \
-  -v "$PWD"/../www:/var/www:rw \
-  -p 1080:80 \
-  -d php70-nginx /sbin/my_init
